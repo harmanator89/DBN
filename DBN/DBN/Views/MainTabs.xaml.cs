242fe9ae99bd88bcea91fd1dba7f15fcac5beef5
Alex Harman <harmanator89@yahoo.com>
2018-03-15 21:29:50 -0400

    Added Tab Master Layout

---
+++ DBN/DBN/Views/MainTabs.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using DBN.Views.Menu;
+
+namespace DBN.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainTabs : TabbedPage
+    {
+        public MainTabs ()
+        {
+            InitializeComponent();
+
+            Children.Add(new Dashboard());
+            Children.Add(new SaveMe());
+            Children.Add(new About());
+            Children.Add(new SettingsPage());
+        }
+    }
+}
