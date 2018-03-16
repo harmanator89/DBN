2ca7774c520e8844211a16652e15548b6cc3219a
Alex Harman <harmanator89@yahoo.com>
2018-03-15 22:37:44 -0400

    Save me page creation

---
+++ DBN/DBN/Helpers/SaveMeModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+#if __IOS__
+using Foundation;
+#endif
+
+namespace DBN.Helpers
+{
+    class SaveMeModel
+    {
+        public class NavigationSMHelp
+        {
+            
+        }
+    }
+}
